06cff15906f76cc23d60395d4712b05936af479c
18127016 <18127016@student.hcmus.edu.vn>
2021-10-19 00:22:03 +0700

    all

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Game from './component/Game';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Game
+    col={20}
+    row={20}
+    numStep={5}
+     />
   </React.StrictMode>,
   document.getElementById('root')
 );
